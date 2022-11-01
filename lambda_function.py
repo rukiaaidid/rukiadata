e01dc32b86f539877e8cf13ed8b4bbaaf9afdef2
rukiaaidid <35967742+rukiaaidid@users.noreply.github.com>
2022-11-01 16:12:46 +0000

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,32 @@
-import boto3
+# Updates pushed from GitHub repository
+import json, csv, boto3, urllib
+from datetime import datetime
 
-dynamodb = boto3.client('dynamodb')
+s3 = boto3.resource('s3')
+s3bucket = "formentries-ra"
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('test-table')
 
 def lambda_handler(event, context):
-  name = event['name']
-  email = event['email']
-  message = event['message']
-  dynamodb.put_item(TableName='test-table', Item=
-{'name':{'S':name},'email':{'S':email}, 'message':{'S':message}})
-  print(email)
+   # Save file to S3 as a backup
+   dt = datetime.now().strftime("%Y%m%d%H%M%S")
+   uploaddate = datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " UTC"
+   filename = dt + "formentries.json"
+   lambda_path = "/tmp/" + filename
+   s3_path = "data/" + filename
+   url = "https://finaltest-ra.s3.amazonaws.com/form.html"
+   response = urllib.request.Request(url=url, headers={'User-Agent': 'Mozilla/5.0'})
+   urldata = urllib.request.urlopen(response).read()
+   data = json.loads(urldata.decode())
+   with open(lambda_path,'w') as outfile:
+      json.dump(data, outfile)
+   s3.meta.client.upload_file(lambda_path, s3bucket, s3_path)
+   # Save information to DynamoDB table
+   name = event['name']
+   email = event['email']
+   message = event['message']
+   dynamodb.put_item(TableName='test-table', Item=
+   {'name':{'S':name},'email':{'S':email}, 'message':{'S':message}})
+   print(email)
+
+   
