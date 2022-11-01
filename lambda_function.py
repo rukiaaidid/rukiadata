643f7c799a5adbb6b59ed772cb3715b296783ae0
rukiaaidid <35967742+rukiaaidid@users.noreply.github.com>
2022-11-01 14:28:47 +0000

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,28 +1,17 @@
-# Updates pushed from GitHub repository
-import json, csv, boto3, urllib
-from datetime import datetime
+import boto3
 
-s3 = boto3.resource('s3')
-s3bucket = "finaltest-ra"
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('test-table')
+dynamodb = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-   # Save file to S3 as a backup
-   dt = datetime.now().strftime("%Y%m%d%H%M%S")
-   uploaddate = datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " UTC"
-   filename = dt + "_surveydata.json"
-   lambda_path = "/tmp/" + filename
-   s3_path = "data/" + filename
-   url = "https://finaltest-ra.s3.amazonaws.com/form.html"
-   response = urllib.request.Request(url=url, headers={'User-Agent': 'Mozilla/5.0'})
-   urldata = urllib.request.urlopen(response).read()
-   data = json.loads(urldata.decode())
-   with open(lambda_path,'w') as outfile:
-      json.dump(data, outfile)
-   s3.meta.client.upload_file(lambda_path, s3bucket, s3_path)
-   # Save information to DynamoDB table
-   objectpath = "https://" + s3bucket + ".s3.amazonaws.com/" + s3_path
-   item = "{'uploaddate': {" + dt + "},'link': { " + s3_path + "}"
-   item2 = "{'uploaddate': '34','link':  'microsoft'}"
-   table.put_item(Item={'uploaddate': uploaddate ,'objectpath': objectpath })
+   id = event['id']
+   Question 1 = event['Question 1']
+   Question 2 = event['Question 1']
+   Question 3 = event['Question 1']
+   Question 4 = event['Question 1']
+   Question 5 = event['Question 1']
+   Question 6 = event['Question 1']
+   Question 7 = event['Question 1']
+   dynamodb.put_item(TableName='test-table', Item={
+      'Question 1':{'S':Question1}, 'Question 2':{'S':Question2},'Question 3':{'S':Question3},
+      'Question 4':{'S':Question4},'Question 5':{'S':Question5},'Question 6':{'S':Question6},
+      'Question 7':{'S':Question7}})
