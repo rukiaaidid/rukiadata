c24e5bc176d1abdbd0e44a855095e6b41d14f726
rukiaaidid <35967742+rukiaaidid@users.noreply.github.com>
2022-11-01 15:08:40 +0000

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,15 +3,9 @@ import boto3
 dynamodb = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-   Question_1 = event['Question 1']
-   Question_2 = event['Question 2']
-   Question_3 = event['Question 3']
-   Question_4 = event['Question 4']
-   Question_5 = event['Question 5']
-   Question_6 = event['Question 6']
-   Question_7 = event['Question 7']
-   dynamodb.put_item(TableName='test-table', Item={
-      'Question 1':{'S':Question1}, 'Question 2':{'S':Question2},'Question 3':{'S':Question3},
-      'Question 4':{'S':Question4},'Question 5':{'S':Question5},'Question 6':{'S':Question6},
-      'Question 7':{'S':Question7}})
-   
+  name = event['name']
+  email = event['email']
+  message = event['message']
+  dynamodb.put_item(TableName='test-table', Item=
+{'name':{'S':name},'email':{'S':email}, 'message':{'S':message}})
+  print(email)
