5e888bb242c35dc3fde8b8c30fb71d0ac6aca64a
rukiaaidid <35967742+rukiaaidid@users.noreply.github.com>
2022-11-03 13:14:34 +0000

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ import boto3
 dynamodb = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-  name = event['name']
+  firstName = event['firstName']
   email = event['email']
   message = event['message']
   dynamodb.put_item(TableName='test-table', Item=
